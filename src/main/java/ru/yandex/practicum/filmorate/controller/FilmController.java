530eafb1c150718de6f43afd4d3c5572bde54753
Руслан Дергачев <dergrus@yandex.ru>
2022-05-09 16:06:45 +0300

    ФЗ 8 спринта (1 ревью)

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,48 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import javax.validation.Valid;
+import javax.validation.ValidationException;
+import java.time.LocalDate;
+import java.util.HashMap;
+
+@RestController
+@Slf4j
+public class FilmController {
+
+    private final HashMap<String, Film> films = new HashMap<>();
+    int idFilm;
+
+    @GetMapping("/films")
+    public HashMap<String, Film> allFilms() {
+        log.info("Запрос списка всех фильмов получен.");
+        return films;
+    }
+
+
+    @PostMapping("/films")
+    public Film create(@Valid @RequestBody Film film) {
+        log.info("Запрос на добавление фильма получен.");
+        if (film.getReleaseDate().isAfter(LocalDate.of(1895, 12, 28))&& film.getDuration()>0
+                && !film.getDescription().isEmpty() && !film.getName().isEmpty()) {
+            createIdFilms();
+            film.setId(idFilm);
+            films.put(film.getName(), film);
+            return films.get(film.getName());
+        }
+        throw new ValidationException("Введите корректные данные фильма");
+    }
+
+    @PutMapping("/films")
+    public void updateFilm(@Valid @RequestBody Film film) {
+        log.info("Запрос на обновление фильма");
+        films.put(film.getName(), film);
+    }
+
+    public int createIdFilms() {
+        return idFilm++;
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,47 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import javax.validation.ValidationException;
+import java.time.LocalDate;
+import java.util.HashMap;
+
+@RestController
+@Slf4j
+public class UserController {
+    private HashMap<String, User> users = new HashMap();
+    int id;
+
+    @GetMapping("/users")
+    public HashMap<String, User> findAll() {
+        log.info("Получен запрос списка пользователей");
+        return users;
+    }
+
+
+    @PostMapping("/users")
+    public User create(@Valid @RequestBody User user) {
+        log.info("Получен запрос на создание нового пользователя");
+            if (user.getBirthday().isBefore(LocalDate.now()) && user.getEmail().contains("@")) {
+                createId();
+                user.setId(id);
+                users.put(user.getLogin(), user);
+                return user;
+            } throw new ValidationException("Введите корректные данные пользователя");
+    }
+
+    @PutMapping("/users")
+    public User updateUser(@Valid @RequestBody User user) {
+        log.info("Получен запрос на обновление пользователя");
+        users.put(user.getLogin(), user);
+        return users.get(user.getLogin());
+    }
+
+    private int createId() {
+        return id++;
+    }
+
+}
