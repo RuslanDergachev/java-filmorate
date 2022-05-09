09e4b2cc19dd1428cee722fb259ec17371a6d9df
Руслан Дергачев <dergrus@yandex.ru>
2022-05-09 13:24:09 +0300

    ФЗ 8 спринта (1 ревью)

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,45 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.HashMap;
+
+@RestController
+@Slf4j
+public class FilmController {
+
+    private final HashMap<Integer, Film> films = new HashMap<>();
+    int idFilm;
+
+    @GetMapping("/films")
+    public HashMap<Integer, Film> allFilms(){
+        log.info("Запрос списка всех фильмов получен.");
+        return films;
+    }
+
+    @Valid
+    @PostMapping("/films")
+    public void create(@RequestBody Film film) {
+        log.info("Запрос на добавление фильма получен.");
+        if (film.getReleaseDate().isBefore(LocalDate.of(1985, 12, 28))) {
+            createIdFilms();
+            film.setId(idFilm);
+            films.put(film.getId(), film);
+        }
+    }
+
+    @Valid
+    @PutMapping("/films")
+    public void updateFilm(@RequestBody Film film) {
+        log.info("Запрос на обновление фильма");
+            films.put(film.getId(), film);
+    }
+
+    public int createIdFilms(){
+        return idFilm++;
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,26 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Data
+@Slf4j
+@EqualsAndHashCode
+public class Film {
+
+    private int id;
+    @NotNull
+    private String name;
+    @Size(min = 1, max = 200)
+    private String description;
+    private LocalDate releaseDate;
+    @Min(1)
+    private int duration;
+
+}
